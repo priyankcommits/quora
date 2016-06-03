82c0a0c3679d97bc35579c98a4acf1ec5eedad85
Pulumati Priyank <pulumati.priyank@gmail.com>
2016-06-03 15:13:38 +0530

    update

--- quoraslim/settings.py
+++ quoraslim/settings.py
@@ -137,9 +137,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'))
 
 
 LOGIN_REDIRECT_URL = '/login/'
